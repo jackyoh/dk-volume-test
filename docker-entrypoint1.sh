b099e0b1732f4e367b4b87611c8f42b0e880f16f
jack <yangj26952@gmail.com>
2017-01-06 07:03:27 +0800

    modify file

--- docker-entrypoint1.sh
+++ docker-entrypoint1.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
-if [ "$1" = 'postgres' ]; then
-	mkdir -p "$PGDATA"
-	chmod 700 "$PGDATA"
-	chown -R postgres "$PGDATA"
+mkdir -p "$PGDATA"
+chmod 700 "$PGDATA"
+chown -R postgres "$PGDATA"
 
-        mkdir -p "$PGDATA1"
-	chmod 700 "$PGDATA1"
-        chown -R postgres "$PGDATA1"
-        echo "aaaaa">$PGDATA1/aaaa.txt
+mkdir -p "$PGDATA1"
+chmod 700 "$PGDATA1"
+chown -R postgres "$PGDATA1"
+echo "aaaaa">$PGDATA1/aaaa.txt
+echo "bbbbb">$PGDATA1/bbbb.txt
 
-	eval "gosu postgres initdb $POSTGRES_INITDB_ARGS"
-	exec gosu postgres "$@"
-fi
-
+/opt/apache-tomcat-7.0.73/bin/catalina.sh run
+ln -s /opt/apache-tomcat-7.0.73/webapps/ $PGDATA1
 exec "$@"
